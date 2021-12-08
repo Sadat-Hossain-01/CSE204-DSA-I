231b36bb088f576fc6cbda714a96755cd60a1e2e
Sadat-Hossain-01 <sadatho2014@gmail.com>
2021-12-08 23:16:57 +0600

    Update Bank.cpp

--- Offlines/Queue/C++/Bank.cpp
+++ Offlines/Queue/C++/Bank.cpp
@@ -9,6 +9,8 @@ using namespace std;
 
 const bool isCommentOn = true;
 int current_time, idx, done;
+int* enter_time;
+int* service_time;
 
 class Event;
 
@@ -28,21 +30,42 @@ class Event {
   }
 };
 
-void startService(Queue<Event>& q, int& next_finish, bool& isOn) {
+void startService(Queue<Event>& q, int& next_finish, bool& isOn, int which) {
   int id = q.frontValue().index;
   next_finish = current_time + service_time[id];
   isOn = true;
   if (isCommentOn) {
     cout << "Time " << current_time << ": Person " << id + 1
-         << " has started taking service in queue 1" << endl;
+         << " has started taking service in queue " << which << endl;
+  }
+}
+
+void finishService(Queue<Event>& my, Queue<Event>& other, int& lastTime,
+                   bool& isOn, int which) {
+  int id = my.dequeue().index;
+  if (isCommentOn) {
+    cout << "Time " << current_time << ": Person " << id + 1
+         << " has finished from queue " << which << endl;
+  }
+  done++;
+  lastTime = current_time;
+  isOn = false;
+
+  // checking queue switching
+  while (other.length() - my.length() >= 2) {
+    auto leave = other.leaveQueue();
+    if (isCommentOn)
+      cout << "Time " << current_time << ": Person " << leave.index + 1
+           << " has switched to queue " << which << endl;
+    my.enqueue(leave.toggle());
   }
 }
 
 int main() {
   int n;
   cin >> n;
-  int* enter_time = new int[n];
-  int* service_time = new int[n];
+  enter_time = new int[n];
+  service_time = new int[n];
 
   for (int i = 0; i < n; i++) {
     cin >> enter_time[i] >> service_time[i];
@@ -75,64 +98,17 @@ int main() {
       }
     }
 
-    if (!q1_on && q1.length() > 0) {
-      int id = q1.frontValue().index;
-      q1_next = current_time + service_time[id];
-      q1_on = true;
-      if (isCommentOn) {
-        cout << "Time " << current_time << ": Person " << id + 1
-             << " has started taking service in queue 1" << endl;
-      }
-    }
-
-    if (!q2_on && q2.length() > 0) {
-      int id = q2.frontValue().index;
-      q2_next = current_time + service_time[id];
-      q2_on = true;
-      if (isCommentOn) {
-        cout << "Time " << current_time << ": Person " << id + 1
-             << " has started taking service in queue 2" << endl;
-      }
-    }
+    if (!q1_on && q1.length() > 0) startService(q1, q1_next, q1_on, 1);
+    if (!q2_on && q2.length() > 0) startService(q2, q2_next, q2_on, 2);
 
     if (q1_on && current_time == q1_next) {
-      int id = q1.dequeue().index;
-      if (isCommentOn) {
-        cout << "Time " << current_time << ": Person " << id + 1
-             << " has finished from queue 1" << endl;
-      }
-      done++;
-      q1_last_time = current_time;
-      q1_on = false;
-
-      // checking queue switching
-      while (q2.length() - q1.length() >= 2) {
-        auto leave = q2.leaveQueue();
-        if (isCommentOn)
-          cout << "Time " << current_time << ": Person " << leave.index + 1
-               << " has switched to queue " << 1 << endl;
-        q1.enqueue(leave.toggle());
-      }
+      finishService(q1, q2, q1_last_time, q1_on, 1);
+      if (q1.length() > 0) startService(q1, q1_next, q1_on, 1);
     }
 
     if (q2_on && current_time == q2_next) {
-      int id = q2.dequeue().index;
-      if (isCommentOn) {
-        cout << "Time " << current_time << ": Person " << id + 1
-             << " has finished from queue 2" << endl;
-      }
-      done++;
-      q2_last_time = current_time;
-      q2_on = false;
-
-      // checking queue switching
-      while (q1.length() - q2.length() >= 2) {
-        auto leave = q1.leaveQueue();
-        if (isCommentOn)
-          cout << "Time " << current_time << ": Person " << leave.index + 1
-               << " has switched to queue " << 2 << endl;
-        q2.enqueue(leave.toggle());
-      }
+      finishService(q2, q1, q2_last_time, q2_on, 2);
+      if (q2.length() > 0) startService(q2, q2_next, q2_on, 2);
     }
     int min_val =
         min(min(q1_next, q2_next), (idx < n ? enter_time[idx] : INT_MAX));
