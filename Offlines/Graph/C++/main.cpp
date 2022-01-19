30d5067ebc02cd9eb48f2255ed419aaabc55ee56
Sadat-Hossain-01 <sadatho2014@gmail.com>
2022-01-19 19:59:50 +0600

    updated using vector instead of arrays

--- Offlines/Graph/C++/main.cpp
+++ Offlines/Graph/C++/main.cpp
@@ -1,74 +1,70 @@
 #include <iostream>
+#include <numeric>
 #include <queue>
 #include <stack>
 #include <vector>
 using namespace std;
 
 #define endl "\n"
-
 const bool fileIO = true, isCommentOn = false;
-int n, x, l, s;
 const int MAX_SIZE = 10005;
-int ladder_snake[MAX_SIZE];
-int parent[MAX_SIZE];
-int dist[MAX_SIZE];
+const int INF = 2e9;
 
-void reset() {
-  for (int i = 0; i < MAX_SIZE; i++) {
-    parent[i] = -1;
-    ladder_snake[i] = i;
-    dist[i] = 1e8;
-  }
+void reset(int x, vector<int>& dist, vector<int>& parent,
+           vector<int>& ladder_snake, vector<bool>& pushed) {
+  // this function resets all the vectors at the start of each test case
+  fill(parent.begin(), parent.begin() + x + 2, -1);
   dist[0] = -1;
+  fill(dist.begin() + 1, dist.begin() + x + 2, INF);
+  fill(pushed.begin(), pushed.begin() + x + 2, false);
+  // iota fills the vector with 0, 1, 2, 3, 4.....
+  // (consecutive numbers starting from the third argument of the function)
+  iota(ladder_snake.begin(), ladder_snake.begin() + x + 2, 0);
 }
 
-void visit(int node, int immediate_parent, int ancestor) {
-  if (isCommentOn)
-    cout << "Child: " << node << " Parent: " << immediate_parent << endl;
-  parent[node] = immediate_parent;
-  dist[node] = dist[ancestor] + 1;
+inline void setParent(int node, int p, vector<int>& parent) {
+  if (isCommentOn) cout << "\tSet parent[" << node << "] = " << p << endl;
+  parent[node] = p;
 }
 
-void bfs(int source) {
+inline void setDistance(int node, int d, vector<int>& dist) {
+  if (isCommentOn) cout << "\tSet dist[" << node << "] = " << d << endl;
+  dist[node] = d;
+}
+
+inline void setBoth(int node, int p, int d, vector<int>& parent,
+                    vector<int>& dist) {
+  setParent(node, p, parent);
+  setDistance(node, d, dist);
+}
+
+void bfs(int node, int n, int x, vector<bool>& pushed, vector<int>& dist,
+         vector<int>& parent, vector<int>& ladder_snake) {
   queue<int> q;
-  q.push(source);
-  dist[source] = 0;
-  parent[source] = 1;
+  q.push(node);
+  pushed[node] = true;
+  setBoth(node, node, 0, parent, dist);
   while (!q.empty()) {
     int current = q.front();
+    int roll = dist[current] + 1;
     q.pop();
+    if (isCommentOn) cout << "Popped " << current << endl;
     for (int i = 1; i <= min(n, x - current); i++) {
       int destination = current + i;
-      if (parent[destination] != -1)
-        continue;  // destination is already
-                   // visited
-      int prev = current;
-      while (ladder_snake[destination] !=
-             destination) {  // if there is any snake or ladder in this cell
-        if (isCommentOn) cout << destination << endl;
-        visit(destination, prev, current);
-        prev = destination;
+      if (parent[destination] != -1) continue;
+      int previous = current;
+      while (destination != ladder_snake[destination]) {
+        setBoth(destination, previous, roll, parent, dist);
+        previous = destination;
         destination = ladder_snake[destination];
       }
-      visit(destination, prev, current);
-      q.push(destination);
-    }
-  }
-}
 
-void dfs(int node, int par = 0, int ancestor = 0) {
-  visit(node, par, ancestor);
-  for (int i = 1; i <= min(n, x - node); i++) {
-    int destination = node + i;
-    if (dist[destination] > dist[node] + 1) {
-      dist[destination] = dist[node] + 1;
-      int prev = node;
-      while (ladder_snake[destination] != -1) {
-        visit(destination, prev, node);
-        prev = destination;
-        destination = ladder_snake[destination];
+      if (!pushed[destination]) {
+        setBoth(destination, previous, roll, parent, dist);
+        q.push(destination);
+        pushed[destination] = true;
+        if (isCommentOn) cout << "\tPushed " << destination << endl;
       }
-      dfs(destination, prev, node);
     }
   }
 }
@@ -79,26 +75,44 @@ int main() {
     freopen("out.txt", "w", stdout);
   }
 
+  vector<int> ladder_snake(MAX_SIZE);
+  vector<int> dist(MAX_SIZE, INF);
+  vector<int> parent(MAX_SIZE, -1);
+  vector<bool> pushed(MAX_SIZE, false);
+
   int t;
   cin >> t;
 
   while (t--) {
-    reset();
+    int n, x, l, s;
     cin >> n >> x >> l;
+    reset(x, dist, parent, ladder_snake, pushed);
+
     for (int i = 1; i <= l; i++) {
       int ls, le;
       cin >> ls >> le;
       ladder_snake[ls] = le;
     }
+
     cin >> s;
     for (int i = 1; i <= s; i++) {
       int ss, se;
       cin >> ss >> se;
       ladder_snake[ss] = se;
     }
-    bfs(1);
-    // dfs(1);
 
+    // does the bfs call
+    bfs(1, n, x, pushed, dist, parent, ladder_snake);
+
+    // optional printing for debugging
+    if (isCommentOn) {
+      for (int i = 1; i <= x; i++) {
+        cout << "Node: " << i << " Parent: " << parent[i]
+             << " LS: " << ladder_snake[i] << " Distance: " << dist[i] << endl;
+      }
+    }
+
+    // this block prints the number of rolls and the desired path (if any)
     if (parent[x] == -1)
       cout << -1 << endl << "No solution" << endl;
     else {
@@ -119,6 +133,7 @@ int main() {
       cout << endl;
     }
 
+    // this block prints the unreachable nodes if the journey is started from 1
     int unreachable = 0;
     for (int i = 1; i <= x; i++) {
       if (parent[i] == -1) {
@@ -126,8 +141,9 @@ int main() {
         cout << i << " ";
       }
     }
+
     if (unreachable == 0) cout << "All reachable";
-    cout << endl;
+    cout << endl << endl;
   }
 
   return 0;
