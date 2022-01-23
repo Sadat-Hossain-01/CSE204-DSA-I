9a084b53fa6c49ff80c780b10bc1d7453d446bfa
Sadat-Hossain-01 <sadatho2014@gmail.com>
2022-01-24 00:30:03 +0600

    Update main.cpp

--- Offlines/Divide & Conquer/main.cpp
+++ Offlines/Divide & Conquer/main.cpp
@@ -21,7 +21,7 @@ vector<int> generateRandomVector(int index, int seed) {
 
 int main() {
   for (int i = 0; i < length.size(); i++) {
-    int ms = 0, qs = 0, rqs = 0, is = 0, qsi = 0, rqsi = 0, stls = 0;
+    float ms = 0, qs = 0, rqs = 0, is = 0, qsi = 0, rqsi = 0, stls = 0;
     for (int repeat = 1; repeat <= 20; repeat++) {
       int seed = rand();
 
@@ -74,13 +74,14 @@ int main() {
       if (commentOn) cout << "STL sort done" << endl;
     }
     cout << "n = " << length[i] << endl;
-    cout << "Merge Sort: " << ms * 50 << endl;
-    cout << "Quick Sort: " << qs * 50 << endl;
-    cout << "Randomized Quick Sort: " << rqs * 50 << endl;
-    cout << "Insertion Sort: " << is * 50 << endl;
-    cout << "Quick Sort with Sorted Input: " << qsi * 50 << endl;
-    cout << "Randomized Quick Sort with Sorted Input: " << rqsi * 50 << endl;
-    cout << "STL Sort : " << stls * 50 << endl;
+    cout << "Merge Sort: " << ms * 50 << "ms" << endl;
+    cout << "Quick Sort: " << qs * 50 << "ms" << endl;
+    cout << "Randomized Quick Sort: " << rqs * 50 << "ms" << endl;
+    cout << "Insertion Sort: " << is * 50 << "ms" << endl;
+    cout << "Quick Sort with Sorted Input: " << qsi * 50 << "ms" << endl;
+    cout << "Randomized Quick Sort with Sorted Input: " << rqsi * 50 << "ms"
+         << endl;
+    cout << "STL Sort : " << stls * 50 << "ms" << endl;
     cout << endl;
   }
 }
