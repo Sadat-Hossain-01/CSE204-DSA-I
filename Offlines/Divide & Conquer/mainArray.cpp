ff4749eeac4b84e77bc70ee47b8cbba260db473d
Sadat-Hossain-01 <sadatho2014@gmail.com>
2022-01-27 16:06:58 +0600

    .

--- Offlines/Divide & Conquer/mainArray.cpp
+++ Offlines/Divide & Conquer/mainArray.cpp
@@ -10,19 +10,27 @@
 using namespace std;
 
 const int REPEAT = 20;
-const bool commentOn = false;
+const bool commentOn = true;
 const bool sortedCheck = false;
 
-inline std::chrono::high_resolution_clock::time_point getCurrent() {
+inline std::chrono::high_resolution_clock::time_point getCurrentHighResClock() {
   return std::chrono::high_resolution_clock::now();
 }
 
 inline double getElapsedTime(
     const std::chrono::high_resolution_clock::time_point& t1) {
-  auto t2 = getCurrent();
+  auto t2 = getCurrentHighResClock();
   auto time_span =
       std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1);
-  return time_span.count() / 1e6;  // ms
+  return (double)time_span.count() * 1e-6;  // ms
+}
+
+inline clock_t getCurrentClock() { return clock(); }
+
+inline double passedTime(const clock_t& t1) {
+  auto t2 = getCurrentClock();
+  double time = (double)(t2 - t1) / (double)CLOCKS_PER_SEC;
+  return time * 1e3;  // ms
 }
 
 vector<int> length{5, 10, 100, 1000, 10000, 100000};
@@ -39,7 +47,7 @@ void copyArray(int* from, int* to, int len) {
 }
 
 int main() {
-  auto start = getCurrent();
+  auto start = getCurrentHighResClock();
   freopen("outArray.csv", "w", stdout);
   cout << "Time required in ms" << endl;
   cout << "n, Merge Sort, Quicksort, Randomized Quicksort, Insertion Sort, "
@@ -59,7 +67,7 @@ int main() {
       generateRandomArray(generatedArrayForThis, i);
 
       copyArray(generatedArrayForThis, arrayToPass, len);
-      auto begin = getCurrent();
+      auto begin = getCurrentHighResClock();
       mergeSort(arrayToPass, 0, len - 1);
       ms += getElapsedTime(begin);
       if (sortedCheck) assert(isSorted(arrayToPass, len));
@@ -68,7 +76,7 @@ int main() {
              << endl;
 
       copyArray(generatedArrayForThis, arrayToPass, len);
-      begin = getCurrent();
+      begin = getCurrentHighResClock();
       quickSort(arrayToPass, 0, len - 1);
       qs += getElapsedTime(begin);
       if (sortedCheck) assert(isSorted(arrayToPass, len));
@@ -77,7 +85,7 @@ int main() {
              << endl;
 
       copyArray(generatedArrayForThis, arrayToPass, len);
-      begin = getCurrent();
+      begin = getCurrentHighResClock();
       randomizedQuickSort(arrayToPass, 0, len - 1);
       rqs += getElapsedTime(begin);
       if (sortedCheck) assert(isSorted(arrayToPass, len));
@@ -86,7 +94,7 @@ int main() {
              << rqs / repeat << "ms" << endl;
 
       copyArray(generatedArrayForThis, arrayToPass, len);
-      begin = getCurrent();
+      begin = getCurrentHighResClock();
       insertionSort(arrayToPass, len);
       is += getElapsedTime(begin);
       if (sortedCheck) assert(isSorted(arrayToPass, len));
@@ -94,7 +102,7 @@ int main() {
         cerr << "Insertion Sort done. Average Until now: " << is / repeat
              << "ms" << endl;
 
-      begin = getCurrent();
+      begin = getCurrentHighResClock();
       quickSort(arrayToPass, 0, len - 1);
       qsi += getElapsedTime(begin);
       if (sortedCheck) assert(isSorted(arrayToPass, len));
@@ -102,7 +110,7 @@ int main() {
         cerr << "Quick Sort on sorted input done. Average Until now: "
              << qsi / repeat << "ms" << endl;
 
-      begin = getCurrent();
+      begin = getCurrentHighResClock();
       randomizedQuickSort(arrayToPass, 0, len - 1);
       rqsi += getElapsedTime(begin);
       if (sortedCheck) assert(isSorted(arrayToPass, len));
@@ -112,7 +120,7 @@ int main() {
             << rqsi / repeat << "ms" << endl;
 
       copyArray(generatedArrayForThis, arrayToPass, len);
-      begin = getCurrent();
+      begin = getCurrentHighResClock();
       sort(arrayToPass, arrayToPass + len);
       stls += getElapsedTime(begin);
       if (sortedCheck) assert(isSorted(arrayToPass, len));
@@ -132,7 +140,7 @@ int main() {
     delete[] arrayToPass;
   }
 
-  cerr << "Program ended in " << getElapsedTime(start) / 1000.0 << " seconds"
+  cerr << "Program ended in " << getElapsedTime(start) / 1e3 << " seconds"
        << endl;
   return 0;
 }
