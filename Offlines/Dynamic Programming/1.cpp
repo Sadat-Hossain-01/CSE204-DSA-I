65c5208090ec090773a9d28a2bd92a2612854672
Sadat-Hossain-01 <sadatho2014@gmail.com>
2022-02-04 10:58:29 +0600

    .

---
+++ Offlines/Dynamic Programming/1.cpp
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+const int NINF = -2e9;
+#define endl "\n"
+
+inline void max_self(int &a, int b) { a = max(a, b); }
+
+template <typename T>
+void printTable(const vector<vector<T>> &vec) {
+  for (int i = 0; i < vec.size(); i++) {
+    for (int j = 0; j < vec[i].size(); j++) cerr << vec[i][j] << " ";
+    cerr << endl;
+  }
+}
+
+int main() {
+  string a, b;
+  cin >> a >> b;
+  int m = a.size();
+  int n = b.size();
+  vector<vector<int>> dp(m + 1, vector<int>(n + 1, NINF));
+  // LCS Length
+  dp[0][0] = 0;
+  for (int i = 0; i <= m; i++) {
+    for (int j = 0; j <= n; j++) {
+      if (i == 0 || j == 0)
+        dp[i][j] = 0;
+      else {
+        max_self(dp[i][j], max(dp[i - 1][j], dp[i][j - 1]));
+        if (a[i - 1] == b[j - 1]) max_self(dp[i][j], dp[i - 1][j - 1] + 1);
+      }
+    }
+  }
+  // LCS Retrieving
+  string ans = "";
+  int ax = m, bx = n;
+  while (ax && bx) {
+    if (dp[ax][bx] == dp[ax - 1][bx - 1] + 1) {
+      ans.push_back(a[ax - 1]);
+      ax--;
+      bx--;
+    } else if (dp[ax - 1][bx] >= dp[ax][bx - 1])
+      ax--;
+    else
+      bx--;
+  }
+  reverse(ans.begin(), ans.end());
+  assert(ans.size() == dp[m][n]);
+  //    printTable(dp);
+  cout << dp[m][n] << endl << ans << endl;
+}
---
+++ Offlines/Dynamic Programming/2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int INF = 2e9;
+#define endl "\n"
+
+inline void min_self(int &a, int b) { a = min(a, b); }
+
+template <typename T>
+void printTable(const vector<vector<T>> &vec) {
+  for (int i = 0; i < vec.size(); i++) {
+    for (int j = 0; j < vec[i].size(); j++) cerr << vec[i][j] << " ";
+    cerr << endl;
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<vector<int>> cost(n, vector<int>(n));
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++) cin >> cost[i][j];
+  vector<vector<int>> dp(n, vector<int>(1 << n, INF));
+  for (int i = 0; i < n; i++) {
+    dp[i][1 << i] = cost[i][i];
+    for (int mask = 0; mask < (1 << n); mask++) {
+      for (int bit = 0; bit < n; bit++) {
+        if (bit == i) continue;
+        if (!(mask & (1 << bit))) {
+          // city bit is unvisited
+          int charge = 0;
+          for (int j = 0; j < n; j++) {
+            if (mask & (1 << j)) charge += cost[bit][j];
+          }
+          min_self(dp[bit][mask | (1 << bit)], dp[i][mask] + charge);
+        }
+      }
+    }
+  }
+  int ans = INT_MAX;
+  for (int i = 0; i < n; i++) min_self(ans, dp[i][(1 << n) - 1]);
+  printTable(dp);
+  cout << ans << endl;
+}
