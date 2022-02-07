d25642eef8cc53791e398f8e11ec5b6431430151
Sadat-Hossain-01 <sadatho2014@gmail.com>
2022-02-07 12:29:14 +0600

    Update 1.cpp

--- Offlines/Dynamic Programming/1.cpp
+++ Offlines/Dynamic Programming/1.cpp
@@ -31,8 +31,10 @@ int main() {
       if (i == 0 || j == 0)
         dp[i][j] = 0;
       else {
-        max_self(dp[i][j], max(dp[i - 1][j], dp[i][j - 1]));
-        if (a[i - 1] == b[j - 1]) max_self(dp[i][j], dp[i - 1][j - 1] + 1);
+        if (a[i - 1] != b[j - 1])
+          max_self(dp[i][j], max(dp[i - 1][j], dp[i][j - 1]));
+        else
+          max_self(dp[i][j], dp[i - 1][j - 1] + 1);
       }
     }
   }
