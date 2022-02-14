3bc2955fd8a71e5a320f5645fc029829fc19ce64
Sadat-Hossain-01 <sadatho2014@gmail.com>
2022-02-14 21:56:01 +0600

    .

---
+++ Final Online/1.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+typedef long long ll;
+
+const int INF = 2e9;
+const int NINF = -2e9;
+
+template <typename T>
+inline void min_self(T &a, T b) {
+  a = min(a, b);
+}
+template <typename T>
+inline void max_self(T &a, T b) {
+  a = max(a, b);
+}
+
+void solve() {}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t = 1;
+  // cin >> t;
+  for (int tc = 1; tc <= t; tc++) {
+    // cout << "Case #" << tc << ": ";
+    solve();
+  }
+}
