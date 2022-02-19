0fd51cefd4cf8cfa450b582eb139bd1a5a2df406
Sadat-Hossain-01 <sadatho2014@gmail.com>
2022-02-19 12:07:17 +0600

    Files For ConTest :3

--- Final Online/Template.cpp
+++ Final Online/Template.cpp
@@ -4,25 +4,60 @@ using namespace std;
 #define endl "\n"
 typedef long long ll;
 
-const int INF = 2e9;
-const int NINF = -2e9;
+void __print(int x) { cerr << x; }
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char* x) { cerr << '\"' << x << '\"'; }
+void __print(const string& x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
 
-template <typename T>
-inline void min_self(T &a, T b) {
-  a = min(a, b);
+template <typename T, typename V>
+void __print(const pair<T, V>& x) {
+  cerr << '{';
+  __print(x.first);
+  cerr << ',';
+  __print(x.second);
+  cerr << '}';
 }
 template <typename T>
-inline void max_self(T &a, T b) {
-  a = max(a, b);
+void __print(const T& x) {
+  int f = 0;
+  cerr << '{';
+  for (auto& i : x) cerr << (f++ ? "," : ""), __print(i);
+  cerr << "}";
 }
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v) {
+  __print(t);
+  if (sizeof...(v)) cerr << ", ";
+  _print(v...);
+}
+
+#ifndef ONLINE_JUDGE
+#define debug(x...)             \
+  cerr << "[" << #x << "] = ["; \
+  _print(x)
+#else
+#define debug(x...)
+#endif
 
 void solve() {}
 
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
-  int t = 1;
-  // cin >> t;
+
+  int t;
+  cin >> t;
+
   for (int tc = 1; tc <= t; tc++) {
     // cout << "Case #" << tc << ": ";
     solve();
