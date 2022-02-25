e797e222109ecdeec949a7c0205c9f5794166ada
Sadat-Hossain-01 <sadatho2014@gmail.com>
2022-02-25 08:38:35 +0600

    .

--- Final Online/2.cpp
+++ Final Online/2.cpp
@@ -49,17 +49,50 @@ void _print(T t, V... v) {
 #define debug(x...)
 #endif
 
-void solve() {}
+bool on = true;
+int m, n;
+
+ll kruskal(const vector<pair<int, pair<int, int>>>& edges) {
+  vector<int> tree_id(m);
+  iota(tree_id.begin(), tree_id.end(), 0);
+  ll min_cost = 0;
+  for (const auto& e : edges) {
+    if (tree_id[e.second.first] != tree_id[e.second.second]) {
+      min_cost += e.first;
+
+      int old_id = tree_id[e.second.first];
+      int new_id = tree_id[e.second.second];
+      for (int i = 0; i < m; i++) {
+        if (tree_id[i] == old_id) tree_id[i] = new_id;
+      }
+    }
+  }
+  return min_cost;
+}
+
+void solve() {
+  cin >> m >> n;
+  if (m == n && m == 0) {
+    on = false;
+    return;
+  }
+  ll total_length = 0;
+  vector<pair<int, pair<int, int>>> edges(n);
+  for (int i = 0; i < n; i++) {
+    int x, y, z;
+    cin >> x >> y >> z;
+    edges[i] = {z, {x, y}};
+    total_length += z;
+  }
+  sort(edges.begin(), edges.end());
+  cout << total_length - kruskal(edges) << endl;
+}
 
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
-  int t;
-  cin >> t;
-
-  for (int tc = 1; tc <= t; tc++) {
+  while (on)
     // cout << "Case #" << tc << ": ";
     solve();
-  }
 }
